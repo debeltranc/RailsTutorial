e53a803b240b96a6958674efe907ea79e6568d80
David <debeltranc@unal.edu.co>
2017-04-04 00:18:48 +0000

    Setting the Application Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  #get 'welcome/index' tells Rails to map requests to http://ip:port/welcome/index to the welcome controller's index action.
+  get 'welcome/index'
+ 
+  #root 'welcome#index' tells Rails to map requests to the root of the application to the welcome controller's index action
+  root 'welcome#index'
 end
