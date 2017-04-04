1f02b089458a6780b66534ecaa8db547752647a7
David <debeltranc@unal.edu.co>
2017-04-04 00:21:53 +0000

    Added Article resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   #get 'welcome/index' tells Rails to map requests to http://ip:port/welcome/index to the welcome controller's index action.
   get 'welcome/index'
  
+  resources :articles
+ 
   #root 'welcome#index' tells Rails to map requests to the root of the application to the welcome controller's index action
   root 'welcome#index'
 end
